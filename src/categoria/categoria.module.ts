e698b6bd0047a513f927ae43e682d736c4a1ec03
Daniel Nascimento <d.nasciimento@live.com>
2026-03-17 12:12:04 -0300

    ✨ New Feature:  Criação  do Recurso Produto com Relacionamento

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -8,6 +8,6 @@ import { CategoriaService } from './services/categoria.sevice';
   imports: [TypeOrmModule.forFeature([Categoria])],
   providers: [CategoriaService],
   controllers: [CategoriaController],
-  exports: [],
+  exports: [CategoriaService],
 })
 export class CategoriaModule {}
